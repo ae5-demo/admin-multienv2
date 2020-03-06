c7f40085fa49881cebf56d99cabc7ad46f7ba95f
admin <admin@anaconda.com>
2020-03-06 00:11:28 +0000

    added env

--- scripts/prep.sh
+++ scripts/prep.sh
@@ -1,6 +1,5 @@
 # update anaconda-project and prepare all envs
 conda update anaconda-project -y && anaconda-project prepare --all
-conda init
 source ~/.bashrc
 conda activate Julia
 
@@ -10,7 +9,10 @@ julia scripts/ijulia.jl
 # install kernel in local jupyter
 jupyter kernelspec install /opt/continuum/.local/share/jupyter/kernels/julia-1.1 --prefix $CONDA_PREFIX
 
-#remove conda init from ~/.bashrc
-sed -i '/^# >>>/,/# <<</d' ~/.bashrc
+# export kname="py37" ;   more /opt/continuum/anaconda/envs/py37/share/jupyter/kernels/python/kernel.json | sed s/\"display_name\":\ .*/\"display_name\":\ \"$kname\",/
+export kenv="py37"
+export pyver="3"
+sed -i s/\"display_name\":\ .*/\"display_name\":\ \"$kname\",/ /opt/continuum/anaconda/envs/$kenv/share/jupyter/kernels/python$pyver/kernel.json
+
 
 
