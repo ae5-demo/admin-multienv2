761e455f0578408e8e8034a0069b6a45a7f49936
admin <admin@anaconda.com>
2020-03-10 21:41:28 +0000

    more commands

--- sample_proxy/server.sh
+++ sample_proxy/server.sh
@@ -7,4 +7,26 @@ server() {
 }
 
 cd  sample_proxy
+echo '<H1>Hellow World!! Environment Report...</H1>' > index.html
+date >> index.html
+echo '<H2>Conda Packages</H2>' >> index.html
+conda list >> index.html
+echo '<H2>Conda info</H2>' >> index.html
+conda info >> index.html
+echo '<H2>Conda envs</H2>' >> index.html
+conda env list >> index.html
+echo '<H2>Python-executable</H2>' >> index.html
+PY=$(which python)
+if [[ -n $PY ]]; then
+  echo "using $PY" >> index.html
+  echo -n "Version: " >> index.html
+  $PY --version >> index.html
+else
+  echo none >> index.html
+fi
+
+
+
+
+
 server 
